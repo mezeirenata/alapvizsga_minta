24d240a536e069d188d7cb778e24e20ba769085c
Renata Mezei <mezei.renata@students.jedlik.eu>
2024-05-17 11:22:48 +0200

    #15 pontszámok elosztva egyenlőre

--- Python/Megoldás/1.feladat/mez.py
+++ Python/Megoldás/1.feladat/mez.py
@@ -1,15 +1,17 @@
-meh = int(input('Méhek száma kaptáronként kb. (50 e - 100 e, ezresekben megadva): '))
-kaptar = int(input('Kaptárok száma: '))
+print('1. feladat:')
+meh = int(input('\tMéhek száma kaptáronként kb. (50 e - 100 e, ezresekben megadva): '))
+kaptar = int(input('\tKaptárok száma: '))
 meh_termeles = 30
 
 n = meh*1000 * kaptar
 termeles = meh_termeles * meh*1000
-print(f'Az összes méh száma: {n} db')
-print(f'Jelenleg összesen várható termelés kaptáronként: {termeles} ml')
+print(f'2. feladat: Az összes méh száma: {n} db')
+print(f'3. feladat: Jelenleg összesen várható termelés kaptáronként: {termeles} ml')
 
 osszes_mez = 0.0
+print('4. feladat:')
 for i in range(1,6):
     osszes_mez += 4.67
-    print(f'{i}. hét: {osszes_mez} ml')
-print(f'Egy méh átlagosan 5 hét alatt {osszes_mez} ml mézet termel.')
+    print(f'\t {i}. hét: {osszes_mez} ml')
+print(f'5. feladat: Egy méh 5 hét alatt {osszes_mez} ml mézet termel.')
 
